ffd7b9be8c68cce55c790ad1314ac356aa83a783
AndresSegura79 <edgarandressegurav@gmail.com>
2024-09-30 23:25:23 -0300

    Cambios en la BD

--- initialize.php
+++ initialize.php
@@ -13,8 +13,16 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-// Crear base de datos si no existe
-$sql = "CREATE DATABASE IF NOT EXISTS $db_name";
+// Borrar la base de datos si existe
+$sql = "DROP DATABASE IF EXISTS $db_name";
+if ($conn->query($sql) === TRUE) {
+    echo "Base de datos '$db_name' eliminada exitosamente.<br>";
+} else {
+    die("Error eliminando la base de datos: " . $conn->error);
+}
+
+// Crear base de datos 
+$sql = "CREATE DATABASE $db_name";
 if ($conn->query($sql) === TRUE) {echo "Base de datos '$db_name' verificada o creada exitosamente.<br>";
 } else {
     die("Error creando la base de datos: " . $conn->error);
@@ -23,18 +31,47 @@ if ($conn->query($sql) === TRUE) {echo "Base de datos '$db_name' verificada o cr
 // Conectarse a la base de datos
 $conn->select_db($db_name);
 
-// Crear tabla 'usuarios' si no existe
-$sql = "CREATE TABLE IF NOT EXISTS usuarios (
+
+// Crear tabla 'roles' 
+$sql = "CREATE TABLE roles (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL
+    )";
+if ($conn->query($sql) === TRUE) {
+    echo "Tabla 'roles' verificada o creada exitosamente.<br>";
+} else {
+    die("Error creando tabla 'roles': " . $conn->error);
+}
+
+// Crear tabla 'usuarios' 
+$sql = "CREATE TABLE usuarios (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL,
-    correo VARCHAR(50),
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    contraseña VARCHAR(255) NOT NULL
+    apellido VARCHAR(30) NOT NULL,
+    usuario VARCHAR(30) NOT NULL UNIQUE,
+    mail VARCHAR(50) NOT NULL UNIQUE,
+    fecha_de_nacimiento DATE, 
+    contraseña VARCHAR(255) NOT NULL,
+    FOREIGN KEY (rol_id) REFERENCES roles(id),
+    estado TINYINT(1) NOT NULL DEFAULT 1
 )";
 if ($conn->query($sql) === TRUE) {
     echo "Tabla 'usuarios' verificada o creada exitosamente.<br>";
 } else {
     die("Error creando tabla 'usuarios': " . $conn->error);
 }
+
+// Crear tabla 'logs' 
+$sql = "CREATE TABLE logs (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY usuario_id REFERENCES usuarios(id),
+    fecha_de_acceso TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Tabla 'logs' verificada o creada exitosamente.<br>";
+} else {
+    die("Error creando tabla 'logs': " . $conn->error);
+}
 $conn->close();
 ?>
+
